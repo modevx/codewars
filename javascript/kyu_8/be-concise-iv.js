d88743c92dd762393feec9d2b97daec4668f3132
admin <admin@admins-MacBook-Pro.local>
2020-03-15 11:16:08 -0400

    add final solution

---
+++ javascript/kyu_8/be-concise-iv.js
@@ -0,0 +1,46 @@
+// Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. You may assume that all array elements are unique.
+
+
+// function find(array, element) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === element) return i;
+//   }
+//   return "Not found";
+// }
+
+// -- SOLUTION --    
+// _attempt #1_
+// const find = (array, element) => {
+//   array.forEach((el, index) => {
+//     if(element === el) { console.log( index ); } 
+//     console.log( 'Not Found' );
+//   });
+// }
+
+// _attempt #2_
+const find = (array, e) => {
+  array.includes(e) ? array.indexOf(e) : 'Not Found';
+};
+
+var array = [2,3,5,7,11];
+
+find(array, 2);
+find(array, 3);
+find(array, 5);
+find(array, 6);
+find(array, 11);
+find(array, 'Frank');
+find(array, 4);
+
+array = [true, "Hello World", false, "Lorem Ipsum", 6, Math.PI];
+find(array, true);
+find(array, 'Hello World');
+find(array, false);
+find(array, 'Lorem Ipsum');
+find(array, 6);
+find(array, Math.PI);
+
+// -----------
+// -- NOTES --
+// -----------
+// forEach() by default returns 'undefined'
